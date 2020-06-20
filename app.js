3f26b1f1d635c40ecdd98e0138f1f67746114bb2
BrandonEJenkins <brandonejenkins@gmail.com>
2020-06-20 11:08:14 -0400

    created employee class and manager engineer sub classes

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Pseudocode
+ */
+
+ // Need to prompt user for information about team manager then info about team members
+    // Need to define roles
+        // Use manager, engineer, intern, main
+    // Need to create role specific questions, once user enters role, the resulting questions should be specific to that role
+    // Prompt user for email id, and role specific info
+ // Need to require inquirer
+ // Need to create HTML page
+ // Need to style HTML page
+ // Need to create html template specific to each role type
+
+
+
